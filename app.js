71962b43fa0d1d2e68c3c0b70b62544c7c645b1d
Jubayer Khan <ijubayerkhan@gmail.com>
2021-02-08 23:05:47 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,71 @@
+const searchBtn = document.getElementById('search-btn');
+const mealList = document.getElementById('meal');
+const mealDetailsContent = document.querySelectorAll('.meal-details-content')
+
+
+searchBtn.addEventListener('click', getMealList);
+mealList.addEventListener('click',getMealRecipe)
+
+
+function getMealList(){
+  let searchInputTxt = document.getElementById('search-input').value.trim();
+    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInputTxt}`)
+    .then(res => res.json())
+    .then(data => {
+       let html ="";
+       if(data.meals){
+       data.meals.forEach(meal => {
+         html +=`
+         <div class = "meal-item" data-id="${meal.idMeal}">
+              <div class = "meal-img">
+                <img src = "${meal.strMealThumb}" alt = "food">
+                <br>
+              </div>
+            <div class = "meal-name">
+            <button class="btn btn-danger recipe-btn">Details</button>
+              <h3>${meal.strMeal}</h3>
+            </div>
+      </div>
+       `;
+       });
+        mealList.classList.remove('notFound')
+       }else{
+         html="Please ! Write a valid name";
+         mealList.classList.add('notFound')
+       }
+
+       mealList.innerHTML = html;
+    });
+}
+
+
+function getMealRecipe(a){
+  a.preventDefault();
+  if(a.target.classList.contains('recipe-btn')){
+let mealItem=a.target.parentElement.parentElement;
+fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealItem.dataset.id}`)
+.then(res=>res.json())
+.then(data=>mealRecipeModal(data.meals))
+
+  }
+}
+
+function mealRecipeModal(meals){
+
+const mealDiv=document.getElementById("meal-details-content");
+mealDiv.classList.remove('meal-details-content')
+meals=meals[0];
+  mealDiv.innerHTML=`
+  
+    <h3>Name:"${meals.strMeal}"</h3>
+        
+      <h4>Category:"${meals.strCategory}"</h4>
+      <div class = "recipe-instruct">
+      <div class = "recipe-meal-img">
+      <img src = "${meals.strMealThumb}" alt = "food">
+        </div>
+              <h3>Instructions:</h3>
+              <p>${meals.strInstructions}</p>
+            </div>
+  `;
+}
